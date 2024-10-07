2004691d9fc52d887eefbe4c941ca8836a4cdfda
Juan Palazzo <j_palazzo@hotmail.com>
2024-10-07 14:55:58 +0200

    lab finalizado

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+// require("dotenv").config()
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const port = 5005
+const projects = require("./data/projects.json")
+const articles = require("./data/articles.json")
+
+app.use(express.static("public"))
+app.use(express.json())
+app.use(morgan("dev"))
+
+
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/blog", (req,res) => {
+  res.sendFile(__dirname + "/views/blog.html")
+})
+
+app.get("/api/projects", (req,res) => {
+  res.json(projects)
+})
+
+app.get("/api/articles", (req,res) => {
+  res.json(articles)
+})
+
+app.get("*", (req,res) => {
+  res.sendFile(__dirname + "/views/not-found.html")
+})
+
+app.listen(5005, () => {})
 
 
 
